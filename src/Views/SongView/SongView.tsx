5e85dd00f264e7fec757c8fd8899d3c1d53521e2
Gabriel von Platen <gabrielvonplaten@gmail.com>
2021-07-05 20:29:55 +0200

    Display user's playlists on the home page.
    More styling is needed

--- src/Views/SongView/SongView.tsx
+++ src/Views/SongView/SongView.tsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import SpotifyWebApi from 'spotify-web-api-node';
+import { useSelector } from 'react-redux';
 import './SongView.sass';
 
+// Components
+import CollectionCards from '../../Components/CollectionCards/CollectionCards';
+
+const spotifyApi = new SpotifyWebApi();
+spotifyApi.setAccessToken(localStorage.getItem('accessToken'));
+
 const SongView: React.FC = () => {
+  const { user } = useSelector((state: any) => state.userData);
+  const [collection, setCollection] = useState<any>(null);
+
+  useEffect(() => {
+    spotifyApi
+      .getUserPlaylists(user.id)
+      .then((data: any) => {
+        setCollection(data.body);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [user]);
+
   return (
     <div className='songView-container'>
-      <h1>Song Title</h1>
-      <h1>Song Title</h1>
-      <h1>Song Title</h1>
-      <h1>Song Title</h1>
-      <h1>Song Title</h1>
-      <h1>Song Title</h1>
-      <h1>Song Title</h1>
-      <h1>Song Title</h1>
-      <h1>Song Title</h1>
-      <h1>Song Title</h1>
-      <h1>Song Title</h1>
-      <h1>Song Title</h1>
+      {collection && (
+        <CollectionCards collection={collection} type='playlists' />
+      )}
     </div>
   );
 };
