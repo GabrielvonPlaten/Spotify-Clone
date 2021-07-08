3636bcd5b6f05d7dbfb7025757130548ebeb6f4e
Gabriel von Platen <gabrielvonplaten@gmail.com>
2021-07-08 17:22:36 +0200

    Add Sidebar home button. Navbar now displays the user's playlists

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import SpotifyWebApi from 'spotify-web-api-node';
 import { Link } from 'react-router-dom';
 import './Navbar.sass';
+
+const spotifyApi = new SpotifyWebApi();
+spotifyApi.setAccessToken(localStorage.getItem('accessToken'));
 
 // Redux
 import { useSelector } from 'react-redux';
@@ -9,13 +13,52 @@ import { useSelector } from 'react-redux';
 import { UserInterface } from '../../Interfaces/UserInterface';
 
 const Navbar: React.FC = () => {
-  const userData = useSelector((state: UserInterface) => state.userData.user);
+  const { user } = useSelector((state: UserInterface) => state.userData);
+  const [playlists, setPlaylists] = useState<any>([]);
+
+  useEffect(() => {
+    getPlaylists();
+  }, [user]);
+
+  const getPlaylists = async () => {
+    try {
+      const res = await spotifyApi.getUserPlaylists(user.id);
+      setPlaylists(res.body.items);
+    } catch (error) {
+      console.log(console.log(error));
+    }
+  };
+
+  useEffect(() => {
+    console.log(playlists);
+  }, [playlists]);
 
   return (
     <header className='navbar'>
       <div className='user_display'>
         {/* <img className='user_display__avatar' src={userData.imageUrl} /> */}
-        <h2 className='user_display__display_name'>{userData.display_name}</h2>
+        <h2 className='user_display__display_name'>{user.display_name}</h2>
+        <div>
+          <div className='navbar-links'>
+            <Link to='/' className='navbar-links__link'>
+              Home
+            </Link>
+          </div>
+          <div className='navbar-playlists-container'>
+            <label>Your Playlists</label>
+            <hr />
+            <ul className='navbar-playlists-list'>
+              {playlists.length > 0 &&
+                playlists.map((item: any, index: number) => (
+                  <li key={index}>
+                    <Link to={`/collection/playlists/${item.id}`}>
+                      <h3>{item.name}</h3>
+                    </Link>
+                  </li>
+                ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </header>
   );
